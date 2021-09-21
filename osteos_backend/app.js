15cda59dbb32222f9734146b7310be4898d856e4
pandasuraj <pandasuraj16@gmail.com>
2021-09-21 12:31:47 +0530

    added script for run and dynamic port

--- osteos_backend/app.js
+++ osteos_backend/app.js
@@ -3,6 +3,7 @@ const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const port = process.env.PORT|| 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -10,6 +11,10 @@ app.use(cors());
 const index = require('./routes/index');
 app.use('/', index);
 
-app.listen(5000, () => {
-     console.log('server is listening on port 5000');
+// app.listen(5000, () => {
+//      console.log('server is listening on port 5000');
+// });
+
+app.listen(port, () => {
+     console.log(`server is listening on port ${port}`);
 });
